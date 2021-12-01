b24f030ee9014da92dcae65f6355cf585e112b6d
Daniel Torok <daniel.torok.dev@gmail.com>
2021-12-01 13:41:50 +0100

    day_01 - haskell

---
+++ day_01/haskell/solution.hs
@@ -0,0 +1,30 @@
+module Solution where
+
+import System.IO ()  
+import Control.Monad ()
+
+slidingWindow :: Num a => [a] -> [a]
+slidingWindow (x:y:z:rest) = (x+y+z) : slidingWindow (y:z:rest)
+slidingWindow rest = []
+
+numberOfDecreases :: (Num a, Ord a) => [a] -> Int
+numberOfDecreases (x:xs) = fst $ foldl fn (0, x) xs
+  where
+    fn :: (Num a, Ord a) => (Int, a) -> a -> (Int, a)
+    fn (counter, prev) curr
+      | curr > prev = (counter + 1, curr)
+      | otherwise = (counter, curr)
+numberOfDecreases _ = error "empty list"
+
+readInt :: String -> Int
+readInt = read
+
+main :: IO ()
+main = do  
+  contents <- readFile "input.data"
+  let numbers = map readInt $words contents
+  let resultPartOne = numberOfDecreases numbers
+  print resultPartOne
+  let alteredNumbers = slidingWindow numbers
+  let resultPartTwo = numberOfDecreases alteredNumbers
+  print resultPartTwo
