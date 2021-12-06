bda3ba97a325826e892550a830e634cf14e85175
I547886 <daniel.torok@sap.com>
2021-12-06 16:38:27 +0100

    day_06 - go - counter

--- day_06/go/solution.go
+++ day_06/go/solution.go
@@ -7,12 +7,34 @@ import (
 	"strings"
 )
 
-func repeat(val int, times int) []int {
-	slice := make([]int, times)
-	for i := range slice {
-		slice[i] = val
+func initialCounter(line string) []int {
+	rawNumbers := strings.Split(line, ",")
+	counter := make([]int, 9)
+	for _, rawNum := range rawNumbers {
+		num, _ := strconv.Atoi(rawNum)
+		counter[num] += 1
 	}
-	return slice
+	return counter
+}
+
+func iterateDay(counter []int) []int {
+	newCounter := counter[1:]
+	newCounter = append(newCounter, counter[0])
+	newCounter[6] += counter[0]
+	return newCounter
+}
+
+func calculateSolution(counter []int, days int) int {
+	acc := counter
+	for i := 0; i < days; i++ {
+		acc = iterateDay(acc)
+	}
+
+	sum := 0
+	for _, num := range acc {
+		sum += num
+	}
+	return sum
 }
 
 func main() {
@@ -21,39 +43,7 @@ func main() {
 		panic(error)
 	}
 
-	line := strings.TrimSpace(string(data))
-	split := strings.Split(line, ",")
-	numbers := make([]int, 0, 500000000)
-	numbers_2 := make([]int, 0, 300)
-	for _, rawNum := range split {
-		num, _ := strconv.Atoi(rawNum)
-		numbers = append(numbers, num)
-		numbers_2 = append(numbers_2, num)
-	}
-
-	/* part one
-		for i := 0; i < 80; i++ {
-			counter := 0
-			for idx, num := range numbers {
-				newNum := num - 1
-				if newNum == -1 {
-					newNum = 6
-					counter += 1
-				}
-
-				numbers[idx] = newNum
-			}
-			numbers = append(numbers, repeat(8, counter)...)
-		}
-	  fmt.Printf("Result: %d\n", len(numbers))
-	*/
-
-	// Part two
-	dict := []int{-1, 6206821033, 5617089148, 5217223242, 4726100874, 4368232009}
-	sum := 0
-	for _, num := range numbers_2 {
-		sum += dict[num]
-	}
-	fmt.Printf("Result: %d\n", sum)
-
+	counter := initialCounter(strings.TrimSpace(string(data)))
+	fmt.Printf("First day esult: %d\n", calculateSolution(counter, 80))
+	fmt.Printf("Second day esult: %d\n", calculateSolution(counter, 256))
 }
