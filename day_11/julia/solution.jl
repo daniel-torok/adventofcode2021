bec0e9b36732961e518c1638d9029e2e85408b23
Daniel Torok <daniel.torok.dev@gmail.com>
2021-12-11 12:41:48 +0100

    day_11 - julia

---
+++ day_11/julia/solution.jl
@@ -0,0 +1,45 @@
+using DelimitedFiles
+
+deltas = vec([(x, y) for x=-1:1, y=-1:1 if x!=0 || y!=0 ])
+function get_adjacent(matrix, x, y)
+  return [((d_x + x, d_y + y)) for (d_x, d_y) in deltas if checkbounds(Bool, matrix, d_x + x, d_y + y)]
+end
+
+function do_iteration(matrix)
+  flashes = Set(Tuple.(findall(==(10), matrix)))
+  while !isempty(flashes)
+    for (x, y) in get_adjacent(matrix, pop!(flashes)...)
+      if matrix[x, y] < 10 && (matrix[x, y] += 1) == 10
+        push!(flashes, (x, y))
+      end
+    end
+  end
+end
+
+function count_flashes(matrix, iteration)
+  matrix = matrix .+ 0 # copy matrix
+  sum_of_flashes = 0
+  for _ in 1:iteration
+    matrix .+= 1
+    do_iteration(matrix)
+    sum_of_flashes += sum(matrix .== 10)
+    matrix .%= 10
+  end
+  return sum_of_flashes
+end
+
+function find_synced_flashes(matrix)
+  matrix = matrix .+ 0 # copy matrix
+  iteration = 0
+  while sum(matrix) != 0
+    matrix .+= 1
+    do_iteration(matrix)
+    matrix .%= 10
+    iteration += 1
+  end
+  return iteration
+end
+
+matrix = readdlm(dirname(@__FILE__) * "/input.data", ' ', Int)
+println("First part: ", count_flashes(matrix, 100))
+println("Second part: ", find_synced_flashes(matrix))
