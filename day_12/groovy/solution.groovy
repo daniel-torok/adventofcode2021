3d5ff941ddd186134716db169eeef937a3454d59
Daniel Torok <daniel.torok.dev@gmail.com>
2021-12-12 13:48:31 +0100

    day_12 - groovy - part two

--- day_12/groovy/solution.groovy
+++ day_12/groovy/solution.groovy
@@ -1,41 +1,37 @@
 import groovy.transform.Field
 
-def putIfAbsent(coll, key, val) {
-  if (!coll.containsKey(key)) {
-    coll.put(key, val)
-  }
-}
-
-def isAllLowerCase(str) {
-  return str.toCharArray().every(Character::isLowerCase)
-}
-
 @Field
 def graph = new File("input.data").inject([:]) { acc, line ->
   def (left, right) = line.split("-")
-  putIfAbsent(acc, left, [].toSet())
-  putIfAbsent(acc, right, [].toSet())
-  acc[left].add(right)
-  acc[right].add(left)
+  acc.putIfAbsent(left, [].toSet())
+  acc.putIfAbsent(right, [].toSet())
+  if (left != "end" && right != "start") {
+    acc[left].add(right)
+  }
+  if (left != "start" && right != "end") {
+    acc[right].add(left)
+  }
   return acc
 }
 
-def countPaths(current, visited) {
+def countPaths(current, visited, smallTwice) {
   if (current == "end") {
     return 1
   }
 
-  def nexts = graph[current].minus(visited)
+  smallTwice = smallTwice || visited.contains(current)
+  def nexts = graph[current].minus(smallTwice ? visited : [])
   if (nexts.isEmpty()) {
     return 0
   }
 
-  visited = visited.collect().toSet()
-  if (this.isAllLowerCase(current)) {
+  visited = visited.clone()
+  if (current.toLowerCase() == current) {
     visited.add(current)
   }
 
-  return nexts.collect { this.countPaths(it, visited.collect().toSet()) }.sum()
+  return nexts.collect { this.countPaths(it, visited.clone(), smallTwice) }.sum()
 }
 
-println "First: " + countPaths("start", [].toSet())
+println "First: " + countPaths("start", [].toSet(), true)
+println "Second: " + countPaths("start", [].toSet(), false)
