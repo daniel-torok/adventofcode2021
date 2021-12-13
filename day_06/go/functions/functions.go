422a3aff38c0eb5042cdea6e62c837a373f0da89
Daniel Torok <daniel.torok.dev@gmail.com>
2021-12-13 17:49:36 +0100

    day_06 - go - convert tab to spaces

--- day_06/go/functions/functions.go
+++ day_06/go/functions/functions.go
@@ -1,16 +1,16 @@
 package functions
 
 func Sum(coll []int) int {
-	sum := 0
-	for _, val := range coll {
-		sum += val
-	}
-	return sum
+  sum := 0
+  for _, val := range coll {
+    sum += val
+  }
+  return sum
 }
 
 func Rotate(coll []int) []int {
-	tmp := coll[0]
-	coll = coll[1:]
-	coll = append(coll, tmp)
-	return coll
+  tmp := coll[0]
+  coll = coll[1:]
+  coll = append(coll, tmp)
+  return coll
 }
--- day_06/go/solution.go
+++ day_06/go/solution.go
@@ -1,38 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+  "fmt"
+  "os"
+  "strconv"
+  "strings"
 
-	"go/solution/functions"
+  "go/solution/functions"
 )
 
 func initializeCounter(line string) []int {
-	counter := make([]int, 9)
-	for _, rawNum := range strings.Split(line, ",") {
-		num, _ := strconv.Atoi(rawNum)
-		counter[num] += 1
-	}
-	return counter
+  counter := make([]int, 9)
+  for _, rawNum := range strings.Split(line, ",") {
+    num, _ := strconv.Atoi(rawNum)
+    counter[num] += 1
+  }
+  return counter
 }
 
 func calculateSolution(counter []int, days int) int {
-	for i := 0; i < days; i++ {
-		counter[7] += counter[0]
-		counter = functions.Rotate(counter)
-	}
-	return functions.Sum(counter)
+  for i := 0; i < days; i++ {
+    counter[7] += counter[0]
+    counter = functions.Rotate(counter)
+  }
+  return functions.Sum(counter)
 }
 
 func main() {
-	data, error := os.ReadFile("input.data")
-	if error != nil {
-		panic(error)
-	}
-
-	counter := initializeCounter(strings.TrimSpace(string(data)))
-	fmt.Printf("First day result: %d\n", calculateSolution(counter, 80))
-	fmt.Printf("Second day result: %d\n", calculateSolution(counter, 256))
+  data, _ := os.ReadFile("input.data")
+  counter := initializeCounter(strings.TrimSpace(string(data)))
+  fmt.Printf("First day result: %d\n", calculateSolution(counter, 80))
+  fmt.Printf("Second day result: %d\n", calculateSolution(counter, 256))
 }
